56e01bdf74cc9a02d5a91ba725623bb0db207c9b
Gogs <gogs@fake.local>
2019-11-10 20:00:08 +0300

    cleaned up assignment folder

--- simple-sql-parser-short/sql.sql
+++
@@ -1,3 +0,0 @@
-SELECT id, name, address FROM users WHERE is_customer IS NOT NULL ORDER BY created;
-INSERT INTO user_notes (id, user_id, note, created) VALUES (1, 1, "Note 1", NOW());
-DELETE FROM database2.logs WHERE id < 1000;
